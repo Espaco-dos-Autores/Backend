129b664b73f00a195cdff0febfe2182cf1e408a1
dudu822 <81241601+dudu822@users.noreply.github.com>
2022-01-14 19:26:51 -0300

    Update LoginApplication.java

--- src/main/java/me/espacodosautores/login/LoginApplication.java
+++ src/main/java/me/espacodosautores/login/LoginApplication.java
@@ -9,6 +9,18 @@ public class LoginApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(LoginApplication.class, args);
 		
+		for (int a=0; a<8; a++) {
+			System.out.println();
+		}
+		
+		System.out.println("================= ATENÇÂO =================");
+		System.out.println();
+		System.out.println();
+		System.out.println("SERVIDOR Espaço dos autores iniciado:)");
+		System.out.println();
+		System.out.println();
+		System.out.println("==================== # ====================");
+		
 	
 	}
 
